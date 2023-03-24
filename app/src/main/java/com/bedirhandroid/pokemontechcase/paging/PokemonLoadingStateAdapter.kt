1f916ca4798e46f9a354d25f0704c149e1281a4e
Bedirhan Dag <bedirhan.dag@marti.tech>
2023-03-25 00:31:06 +0300

    some changes

--- app/src/main/java/com/bedirhandroid/pokemontechcase/paging/PokemonLoadingStateAdapter.kt
+++ app/src/main/java/com/bedirhandroid/pokemontechcase/paging/PokemonLoadingStateAdapter.kt
@@ -2,13 +2,13 @@ package com.bedirhandroid.pokemontechcase.paging
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.paging.LoadState
 import androidx.paging.LoadStateAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bedirhandroid.pokemontechcase.R
 import com.bedirhandroid.pokemontechcase.databinding.ItemNetworkStateBinding
 import com.bedirhandroid.pokemontechcase.ui.second.fragments.pokemon.list.PokemonListAdapter
+import com.bedirhandroid.pokemontechcase.util.visibleIf
 
 //Pokemon state adapter like -> Loading, Timeout, error
 class PokemonLoadingStateAdapter(
@@ -27,9 +27,9 @@ class PokemonLoadingStateAdapter(
 
         fun bind(loadState: LoadState) {
             with(binding) {
-                progressBar.isVisible = loadState is LoadState.Loading
-                retryButton.isVisible = loadState is LoadState.Error
-                errorMsg.isVisible =
+                progressBar visibleIf  (loadState is LoadState.Loading)
+                retryButton visibleIf  (loadState is LoadState.Error)
+                errorMsg visibleIf
                     !(loadState as? LoadState.Error)?.error?.message.isNullOrBlank()
                 errorMsg.text = (loadState as? LoadState.Error)?.error?.message
             }
